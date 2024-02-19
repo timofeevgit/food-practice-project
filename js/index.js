1d44125ad4540f68389a3f2bb21af5bc594f23df
Dmitry Timofeev <dimadimajob@gmail.com>
2024-02-19 14:19:39 +0300

    add classes

--- js/index.js
+++ js/index.js
@@ -133,7 +133,7 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 
   // модальное окно открывается автоматически через 9 секунд после загрузки сайта
-  const modalTimerId = setTimeout(openModal, 9000)
+  // const modalTimerId = setTimeout(openModal, 9000);
   // модальное окно открывается автоматически когда юзер доскроливает до конца сайта (придется работать с координатами страницы)
   function showModalByScroll() {
       if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -142,5 +142,75 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     };
   window.addEventListener('scroll', showModalByScroll);
-  
+
+  // Используем классы для карточек
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+
+      // Свойство для конвертера валюты
+      this.transfer = 90;
+      this.changeToRUB();
+    }
+
+    // конвертер валюты
+    changeToRUB() {
+      this.price = this.price * this.transfer;
+    }
+
+    // Формируем вёрстку
+    render() {
+      const element = document.createElement('div');
+      element.innerHTML = `
+      <div class="menu__item">
+        <img src=${this.src} alt=${this.alt}>
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+        </div>
+      </div>
+      `;
+
+      this.parent.append(element);
+    }
+  }
+
+  // объект создается и никуда не записывается. Т.е. удаляется после отработки. Удобно, если объект нужно использовать лишь один раз.
+  // 1 карточка
+  new MenuCard(
+    'img/tabs/vegy.jpg',
+    'vegy',
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    '.menu .container'
+  ).render();
+
+  // 2 карточка
+  new MenuCard(
+    'img/tabs/elite.jpg',
+    'elite',
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    12,
+    '.menu .container'
+  ).render();
+
+    // 3 карточка
+    new MenuCard(
+      'img/tabs/post.jpg',
+      'post',
+      'Меню "Постное"',
+      'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+      14,
+      '.menu .container'
+    ).render();
 });
